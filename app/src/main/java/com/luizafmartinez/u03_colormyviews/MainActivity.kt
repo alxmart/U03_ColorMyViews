fc2e238eae037de59c0a37fb54b7900436484f71
alxmart <alxmartinez@gmail.com>
2024-03-06 08:12:52 -0300

    Adding coding to change buttons colors

--- app/src/main/java/com/luizafmartinez/u03_colormyviews/MainActivity.kt
+++ app/src/main/java/com/luizafmartinez/u03_colormyviews/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.luizafmartinez.u03_colormyviews
 
 import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintSet.Layout
+import androidx.core.graphics.convertTo
+import androidx.core.graphics.red
+import androidx.core.graphics.toColor
+import com.luizafmartinez.u03_colormyviews.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         setListeners()
     }
 
-    private fun setListeners() {
+     fun setListeners() {
 
         val box_one_text = findViewById<TextView>(R.id.box_one_text)
         val box_two_text = findViewById<TextView>(R.id.box_two_text)
@@ -25,9 +31,9 @@ class MainActivity : AppCompatActivity() {
         val box_four_text = findViewById<TextView>(R.id.box_four_text)
         val box_five_text = findViewById<TextView>(R.id.box_five_text)
 
-        val red_button = findViewById<TextView>(R.id.box_three_text)
-        val yellow_button = findViewById<TextView>(R.id.box_four_text)
-        val green_button = findViewById<TextView>(R.id.box_five_text)
+        val red_button = findViewById<Button>(R.id.red_button)
+        val yellow_button = findViewById<Button>(R.id.yellow_button)
+        val green_button = findViewById<Button>(R.id.green_button)
 
         val clickableViews: List<View> = listOf( box_one_text,
             box_two_text, box_three_text, box_four_text,
@@ -39,8 +45,8 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+      fun makeColored(view: View) {
 
-    private fun makeColored(view: View) {
         when (view.id) {
 
             // Boxes using Color class colors for background
@@ -53,12 +59,18 @@ class MainActivity : AppCompatActivity() {
             R.id.box_five_text -> view.setBackgroundResource(android.R.color.holo_green_light)
 
             // Boxes using Custom colors for background
-            R.id.box_three_text -> view.setBackgroundResource(R.color.my_red)
-            R.id.box_four_text -> view.setBackgroundResource(R.color.my_yellow)
-            R.id.box_five_text -> view.setBackgroundResource(R.color.my_green)
-          /*  R.id.red_button -> view.setBackgroundResource(@android:color/m)
-            R.id.yellow_button -> view.setBackgroundResource(android.R.color.)
-            R.id.green_button -> view.setBackgroundResource(android.R.color.my_green)*/
+            R.id.red_button -> {
+                val box_three_text = findViewById<TextView>(R.id.box_three_text)
+                box_three_text.setBackgroundResource(R.color.my_red)
+            }
+            R.id.yellow_button -> {
+                val box_four_text = findViewById<TextView>(R.id.box_four_text)
+                box_four_text.setBackgroundResource(R.color.my_yellow)
+            }
+            R.id.green_button -> {
+                val box_five_text = findViewById<TextView>(R.id.box_five_text)
+                box_five_text.setBackgroundResource(R.color.my_green)
+            }
 
             else -> view.setBackgroundColor(Color.LTGRAY)
         }
